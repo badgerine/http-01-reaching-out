1288db5de5893ab9e4c8dffc2ef7a93912071c0e
sibusiso matsini <sbu.matsini@gmail.com>
2019-12-21 13:19:06 +0200

    S9#194 transforming data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,7 +5,6 @@ import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 import axios from 'axios';
-import post from '../../components/Post/Post';
 
 class Blog extends Component {
 
@@ -16,21 +15,24 @@ class Blog extends Component {
     componentDidMount(){
         axios.get('http://jsonplaceholder.typicode.com/posts')
         .then(response => {
-            this.setState({posts: response.data})
+            const posts = response.data.slice(0,4);
+           
+            const updatedPosts = posts.map(post => {
+                return {...post, author:'Sbu'}});
+
+            this.setState({posts: updatedPosts})
             // console.log(response.data);
-        })
+        });
     }
 
     render () {
         const posts = this.state.posts.map(
-            post => <Post key={post.id} title={post.title}/>
+            post => <Post key={post.id} title={post.title} author={post.author}/>
         );
         return (
             <div>
                 <section className="Posts">
                    {posts}
-                    <Post />
-                    <Post />
                 </section>
                 <section>
                     <FullPost />
