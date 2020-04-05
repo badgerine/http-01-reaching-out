79a7944ee17ea5fd5f96d1535c815e918319031b
sibusiso matsini <sbu.matsini@gmail.com>
2020-04-05 19:27:05 +0200

    S11#205 using to switch to load a single route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,11 +3,11 @@ import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 import FullPost from './FullPost/FullPost';
-import { Route, NavLink} from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 class Blog extends Component {
 
-    componentDidMount(){
+    componentDidMount() {
         console.log(this.props);
     }
 
@@ -17,18 +17,18 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><NavLink 
-                                to="/" 
+                            <li><NavLink
+                                to="/"
                                 exact
                                 activeClassName="my-active">Home</NavLink></li>
                             <li><NavLink to={{
                                 pathname: "/new-post",
                                 hash: "#submit",
                                 search: "?quick-submit=true"
-                                }}
+                            }}
                                 activeStyle={{
-                                    color:'#fa590f',
-                                    textDecoration:'underline'
+                                    color: '#fa590f',
+                                    textDecoration: 'underline'
                                 }}
                             >New Post</NavLink></li>
                         </ul>
@@ -36,9 +36,12 @@ class Blog extends Component {
                 </header>
                 {/* <Route path="/" exact render={() => <h1>Home</h1>}/>
                 <Route path="/new-post"  render={() => <h1>Say something funny</h1>}/> */}
-                <Route path="/" exact component={Posts}/>
-                <Route path="/new-post" component={NewPost} />
-                <Route path="/:id" component={FullPost} />
+                <Switch>{/*ensure 1 route is loaded */}
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
+
             </div>
         );
     }
